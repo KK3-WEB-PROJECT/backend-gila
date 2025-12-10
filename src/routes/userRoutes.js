555bad7677846a20957f6980d1d50ce15c6f4a33
Muhamad Salman Firdaus <s92908741@gmail.com>
2025-12-10 09:03:15 +0700

    last

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,13 +5,11 @@ const auth = require('../middleware/authMiddleware');
 const uploadLocal = require("../middleware/uploadLocal");
 
 router.put("/profile/:id", auth, uploadLocal.single("profile"),userController.updateProfileLocal);
-// router.put("/user/:id", auth, uploadLocal.single("photo"),userController.updateProfileLocal);
 router.get("/", auth, userController.getAllUsers);
 router.get("/:id", auth, userController.getUserById);
 router.post("/register", userController.registerUser);
 router.post("/login", userController.loginUser);
 router.put("/:id", auth, userController.updateUser);
-// router.put("/user/:id", auth, userController.updateProfileLocal);
 router.delete("/:id", auth, userController.deleteUser);
 router.delete("/:id/profile", userController.deleteProfile);
 
